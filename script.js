12e7ff31ee44558dfa4d18389bff1e1efbbb958b
Victor Wong <victor.wong@connexionltd.com>
2025-04-17 17:39:24 +1000

    remove logo
    remove log

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const intro = document.getElementById("logo-intro");
-  const mainApp = document.getElementById("main-app");
+// window.addEventListener("DOMContentLoaded", () => {
+//   const intro = document.getElementById("logo-intro");
+//   const mainApp = document.getElementById("main-app");
 
-  // Wait 3s for animations to complete (1s in, 1s hold, 1s out)
-  setTimeout(() => {
-    intro.style.display = "none";
-    // mainApp.classList.remove("hidden");
-  }, 3000);
-});
+//   // Wait 3s for animations to complete (1s in, 1s hold, 1s out)
+//   setTimeout(() => {
+//     intro.style.display = "none";
+//     // mainApp.classList.remove("hidden");
+//   }, 3000);
+// });
 
 // script.js
 const flipSound = new Audio('flip.mp3');
